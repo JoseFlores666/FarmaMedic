ddfd9b9571ab4b3b51f6149702b4c7e76dc577af
Jose Flores <20221043@uthh.edu.mx>
2024-11-22 11:54:30 -0600

    Certificados https

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
-// https://vitejs.dev/config/
+const __dirname = path.resolve();  
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'ssl', 'server.key')),  
+      cert: fs.readFileSync(path.resolve(__dirname, 'ssl', 'server.crt')), 
+    },
+    host: 'localhost',
+    port: 5173,
+  },
 })
